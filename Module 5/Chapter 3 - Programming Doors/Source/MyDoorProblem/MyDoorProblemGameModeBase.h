280c25a67805fae9e8f4a63961aa5d3842658c6f
KristaBooneActiv <krista.boone@activision.com>
2022-08-16 12:22:41 -0400

    Add Module 5, Chapter 3 base project

---
+++ Module 5/Chapter 3 - Programming Doors/Source/MyDoorProblem/MyDoorProblemGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyDoorProblemGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYDOORPROBLEM_API AMyDoorProblemGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
