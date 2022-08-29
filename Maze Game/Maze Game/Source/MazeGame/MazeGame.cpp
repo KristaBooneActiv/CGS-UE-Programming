a8b6c76d329cb5a681cff5c48cd62f5674edcce3
KristaBooneActiv <krista.boone@activision.com>
2022-08-29 19:30:08 -0400

    Remove old projects
    Merged Maze Game and Fox Maze into one game.

--- Maze Game/Maze Game/Source/MazeGame/MazeGame.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "MazeGame.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MazeGame, "MazeGame" );
- 
